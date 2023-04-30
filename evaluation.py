5e8f6db9986df849115b3dbce94342e879446649
Xiaomeng Zhu <106610647+xiaomeng-zhu@users.noreply.github.com>
2023-04-30 17:19:56 -0400

    updated evaluation.py with standard data structure

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,12 @@
 def calculate_mistakes(G, C):
   """
   Given a clustering assignment C and a graph G, calculate the number of mistakes
+  G: a list of lists containing decisions
+  e.g. [[1,1,0],
+        [1,1,1],
+        [0,1,1]] is a decision matrix where only (1,3) the only negative edge, all other edges are positive (indexing from 1
+  C: a set of sets (C_0, C_1, C_2, ....)
+  e.g. ((1,2),(3)) meaning that vertex 1 and vertex 2 are in the same cluster, and vertex 3 is in its singleton cluster
   """
   pass
   
