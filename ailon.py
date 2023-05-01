6988b7863ceb0e879a4161410b8f4f3bdd6d098d
malika07032 <malika07032@gmail.com>
2023-05-01 17:03:54 -0400

    Started ailon algo

--- ailon.py
+++ ailon.py
@@ -1,2 +1,21 @@
-def ailon():
-  pass
+import random
+
+def ailon(V, edges):
+  """
+  For now:
+  V - a set of vertices in the graph
+  edges - an adjacency matrix (dict of lists)
+  """
+  Vt = V
+  #t = 1
+  clusters = set()
+  while len(Vt) != 0:
+    p = random.choice(Vt)
+    C = set()
+    C.add(p)
+    for v in edges[p]:
+      C.add(v)
+    Vt = Vt.difference(C)
+    clusters.add(C)
+
+  return clusters
