59678bc2668ed03d86e6ff88941358da6b65cd68
Xiaomeng Zhu <106610647+xiaomeng-zhu@users.noreply.github.com>
2023-04-30 21:12:48 -0400

    started coding for bansal.py

--- bansal.py
+++ bansal.py
@@ -1,5 +1,13 @@
-def bansal_naive():
-  pass
+def bansal_naive(graph):
+  """
+  return a set of sets (clusterings)
+  """
+  num_pos = 0
+  num_neg = 0
+  for row in graph:
+    for elt in row:
+      pass
 
-def bansal():
+
+def bansal_algorithm_cautious(graph):
   pass
