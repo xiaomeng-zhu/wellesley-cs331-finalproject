48f9f32fa01b02fdf415cfdbcb6ffa10b9ac49ec
malika07032 <malika07032@gmail.com>
2023-05-01 17:11:32 -0400

    Added a simple graph to test the algorithm

--- ailon.py
+++ ailon.py
@@ -8,14 +8,22 @@ def ailon(V, edges):
   """
   Vt = V
   #t = 1
-  clusters = set()
+  #clusters = set()
+  clusters = []
   while len(Vt) != 0:
-    p = random.choice(Vt)
+    p = random.choice(list(Vt))
     C = set()
     C.add(p)
     for v in edges[p]:
       C.add(v)
     Vt = Vt.difference(C)
-    clusters.add(C)
+    print(C)
+    clusters.append(C)
 
   return clusters
+
+
+V = {1, 2, 3, 4}
+edges = {1: [2], 2: [1], 3:[4], 4:[3]}
+
+print(ailon(V, edges))
