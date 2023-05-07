79800d73a11d5e140064bd9b721302117e95a422
Xiaomeng Zhu <106610647+xiaomeng-zhu@users.noreply.github.com>
2023-05-06 23:45:17 -0400

    started implementing algorithm cautious in bansal but not yet debugged

--- bansal.py
+++ bansal.py
@@ -1,3 +1,5 @@
+import random
+
 def get_all_edges(n):
     """
     Input: number of vertices in a complete graph
@@ -10,6 +12,17 @@ def get_all_edges(n):
             if i < j:
                all_edges.append((i,j))
     return all_edges
+
+def get_all_positive_neighbors(G,v):
+    """
+    Input: a decision matrix G and a vertex v
+    Output: a list of all the positive vertices of v
+    """
+    positive_neighbors = []
+    for idx, decision in enumerate(G[v]):
+        if decision == 1:
+            positive_neighbors.append(idx)
+    return positive_neighbors
 
 
 def bansal_naive(graph):
@@ -40,11 +53,57 @@ def bansal_naive(graph):
         # print([[u] for u in list(range(n))])
         return [{u} for u in list(range(n))]
 
-def delta_good(v, C, n):
-    pass
+def delta_good(delta, v, C, G):
+    """
+    Given a vertex, cluster C, set of vertices V, check if v is delta-good
+    note: set of vertices V can be calculated based on G
+    """
+    
+    cluster_set = set(C)
+    positive_neighbors = get_all_positive_neighbors(G,v)
+    positive_neighbors_set = set(positive_neighbors)
 
-def bansal_algorithm_cautious(graph):
-    pass
+    quantity1 = len(positive_neighbors_set.intersect(cluster_set))
+    quantity2 = (1-delta)*len(C)
+
+    # check if it satisfies the first constraint presented on Bansal et al. p.94
+    if quantity1 < quantity2:
+        return False
+    
+    all_vertex_set = set(range(len(G)))
+    quantity3 = len(positive_neighbors_set.intersect(all_vertex_set-cluster_set))
+    quantity4 = delta*len(C)
+
+    # check if it satisfies the second constraint presented on Bansal et al. p.94
+    if quantity3 > quantity4:
+        return False
+    
+    return True
+    
+
+def bansal_algorithm_cautious(G):
+    """
+    Input: a decision matrix G
+    Output: clusters under Algorithm Cautious in Bansal et al. p.97
+    """
+    random_start = random.choice(list(range(len(G))))
+    Av = get_all_positive_neighbors(G, random_start)
+
+    # vertex removal step
+    vertices_to_remove = []
+    for x in Av:
+        if not delta_good(3, random_start, Av, G):
+            vertices_to_remove.append(x)
+
+    Av = list(set(Av) - set(vertices_to_remove))
+
+    # vertex addition step
+    vertices_to_add = []
+    for x in len(G):
+        if delta_good(7, random_start, Av, G):
+            vertices_to_add.append(x)
+    Av = list(set(Av) + set(vertices_to_add))
+    
 
 if __name__ == "__main__":
     simple_graph = [[1,0,0],
--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,26 @@
 from bansal import get_all_edges, bansal_naive
-def in_same_cluster(u,v,C):
+
+def is_valid_decision_matrix(G):
+    """
+    Given a graph G, check if it is valid decision matrix (condition 1 and 2 must all be satisfied)
+    condition 1: the diagonal line from top left to bottom right must all be ones
+    condition 2: the matrix must be symmetric based on the diagonal lines in condition 1
+    """
+    num_v = len(G)
+
+    # check condition 1
+    for i in range(num_v):
+        if G[i][i] != 1: # if numbers on the diagonal line is not 1
+            return False
+    
+    # check condition 2
+    all_edges = get_all_edges(len(G))
+    for u,v in all_edges:
+        if G[u][v] != G[v][u]: # if it is not symmetric
+            return False
+    return True
+
+def is_in_same_cluster(u,v,C):
     """
     Given the indices of vertex u and v and a list of sets C, check if u is in the same cluster as
     as v according to the clustering provided by C
@@ -24,9 +45,9 @@ def calculate_mistakes(G, C):
 
     all_edges = get_all_edges(len(G))
     for u,v in all_edges:
-        if G[u][v] == 0 and in_same_cluster(u,v,C): # u and v should not be in the same cluster
+        if G[u][v] == 0 and is_in_same_cluster(u,v,C): # u and v should not be in the same cluster
             total_mistakes += 1
-        elif G[u][v] == 1 and (not in_same_cluster(u,v,C)):
+        elif G[u][v] == 1 and (not is_in_same_cluster(u,v,C)):
             total_mistakes += 1
     
     correctness_index = total_mistakes/len(all_edges)
@@ -45,10 +66,11 @@ if __name__ == "__main__":
     simple_graph = [[1,1,0],
                     [1,1,1],
                     [0,1,1]]
+    print(is_valid_decision_matrix(simple_graph)) # expected: True
     clusters = bansal_naive(simple_graph)
-    print(clusters)
-    print(calculate_mistakes(simple_graph, clusters))
-    print(calculate_mistakes_under_alg(bansal_naive, simple_graph))
+    print(clusters) # expected: [{0,1,2}]
+    print(calculate_mistakes(simple_graph, clusters)) # expected: 0.33333333
+    print(calculate_mistakes_under_alg(bansal_naive, simple_graph)) # expected: 0.33333333
 
 
   
